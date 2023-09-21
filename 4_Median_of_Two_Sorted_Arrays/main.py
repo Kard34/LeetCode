f9cb743e299f679f0328d8f18f6b7fffc468f878
Kard34 <Kardn0145@gmail.com>
2023-09-21 11:18:22 +0700

    Unsuccessful submission on LeetCode (Wrong output)

--- 4_Median_of_Two_Sorted_Arrays/main.py
+++ 4_Median_of_Two_Sorted_Arrays/main.py
@@ -5,9 +5,19 @@
 
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        
+        Merged_Arrays = []
+        x = (len(nums1) + len(nums2)) // 2
+        while len(nums1) > 0 and len(nums2) > 0:
+            if nums1[0] <= nums2[0]:
+                Merged_Arrays.append(nums1.pop(0))
+            else:
+                Merged_Arrays.append(nums2.pop(0))
+        while len(nums1) > 0:
+            Merged_Arrays.append(nums1.pop(0))
+        while len(nums2) > 0:
+            Merged_Arrays.append(nums2.pop(0))
+        if len(Merged_Arrays) % 2 == 1:
+            return Merged_Arrays[x]
+        else:
+            return (Merged_Arrays[x - 1] + Merged_Arrays[x]) / 2
+
