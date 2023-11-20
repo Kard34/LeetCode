f2df1b0278fd91b4acf9cdb14c789b359ea17af5
Kard34 <Kardn0145@gmail.com>
2023-11-20 22:23:06 +0700

    change a code to send leetcode

--- 1838_Frequency_of_the_Most_Frequent_Element/main.py
+++ 1838_Frequency_of_the_Most_Frequent_Element/main.py
@@ -1,15 +1,14 @@
-IN = list(map(int, input("Enter list : ").split()))
-k = int(input("Enter k : "))
-diff = [], count = 1
-for i in range(0, len(IN) - 1):
-    diff.append(abs(IN[i + 1] - IN[i]))
-diff = sorted(diff, reverse = True) # sort list diff max to min
-# print(diff)
-while(True):
-    x = diff.pop()
-    if k > x:
-        count += 1
-        k -= x
-    if k == 0 or len(diff) == 0 or k < x:
-        break
-print(count)
+class Solution(object):
+    def maxFrequency(self, nums, k):
+        diff = [], count = 1
+        for i in range(0, len(nums) - 1):
+            diff.append(abs(nums[i + 1] - IN[i]))
+        diff = sorted(diff, reverse = True)
+        while(True):
+            x = diff.pop()
+            if k > x:
+                count += 1
+                k -= x
+            if k < x or k <= 0 or len(diff) <= 0:
+                break
+        return count
